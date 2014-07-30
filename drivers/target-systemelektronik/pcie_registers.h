083e8de9d01ac689845702b1781774708009550f
Arne Ruhnau <arne.ruhnau@target-sg.com>
2014-07-30 14:19:20 +0200

    Integrated the FPGA driver into the kernel.
    We want to allocate a large-enough buffer (200ish MB) in a contiguous region, and the API making this possible is not available to kernel modules.

---
+++ drivers/target-systemelektronik/pcie_registers.h
@@ -0,0 +1,17 @@
+#ifndef _PCIE_REGISTERS_H
+#define _PCIE_REGISTERS_H
+
+#define PCIE_ATU_VIEWPORT               0x900
+#define PCIE_ATU_REGION_INBOUND         (0x1 << 31)
+#define PCIE_ATU_REGION_INDEX0          (0x0 << 0)
+#define PCIE_ATU_CR1                    0x904
+#define PCIE_ATU_TYPE_MEM               (0x0 << 0)
+#define PCIE_ATU_CR2                    0x908
+#define PCIE_ATU_ENABLE                 (0x1 << 31)
+#define PCIE_ATU_LOWER_BASE             0x90C
+#define PCIE_ATU_UPPER_BASE             0x910
+#define PCIE_ATU_LIMIT                  0x914
+#define PCIE_ATU_LOWER_TARGET           0x918
+#define PCIE_ATU_UPPER_TARGET           0x91C
+
+#endif
